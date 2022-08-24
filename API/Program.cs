aa438e43931f88e50a515776979b59ce91cbf7bd
mannawar <dearmannawar@gmail.com>
2022-08-24 07:03:19 +0300

    Adding angular bootstrap and cors

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,26 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
-
+ 
+var policyName = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options => 
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName, 
+                    builder => 
+                    {
+                        builder
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .WithOrigins("https://localhost:4200", "http://localhost:4200");
+                    });
 });
 
 builder.Services.AddControllers();
@@ -17,13 +31,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policyName);
 
 app.UseAuthorization();
 
